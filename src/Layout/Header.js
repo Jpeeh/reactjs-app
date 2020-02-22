9ca396d5f093eda9fa99aa10dfdadd35d4c199e6
Branco <jpeeeh@hotmail.com>
2020-02-22 22:26:16 +0000

    header done

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import './Header.css';
-import {Container, Navbar} from 'react-bootstrap';
+import { Navbar, Nav, Row, Container } from 'react-bootstrap';
 
 class Header extends React.Component{
     render(){
         return (
-            <Container>
-                <Navbar expand="lg" variant="light" bg="light">
+            <Row>
+                <Container>
+            <Navbar bg="dark" variant="dark">
                 <Navbar.Brand>Transcriptions</Navbar.Brand>
-                <Navbar.Brand><img alt="Fetch Document" src="../../public/challenge_assets/fetch-document.svg"></img></Navbar.Brand>
-                <Navbar.Brand><img alt="Upload" src="../../public/challenge_assets/upload.svg"></img></Navbar.Brand>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto justify-content-end">
+                        <Navbar.Brand>
+                            <img alt="Upload" src={require('../challenge_assets/upload.svg')}></img>
+                        </Navbar.Brand>
+                        <Navbar.Brand>
+                            <img alt="Fetch" src={require('../challenge_assets/fetch-document.svg')}></img> 
+                        </Navbar.Brand>
+                    </Nav>
+                </Navbar.Collapse>
             </Navbar>
-          </Container>
+            </Container>
+            </Row>
         ) 
     }
 }
